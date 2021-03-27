faa4c10e50de780836bd505cc338a8d02dfe52e7
jpb06 <jpb.06@outlook.com>
2021-03-27 20:09:42 +0100

    Global

--- src/logic/updatePackage.ts
+++ src/logic/updatePackage.ts
@@ -8,8 +8,10 @@ export const updatePackage = async (mask: Array<number>): Promise<string> => {
   const packageJson = await readPackage();
   const newVersion = getNewVersion(mask, packageJson.version);
 
-  await exec(`git config user.name ${context.actor}`);
-  await exec(`git config user.email ${context.actor}@users.noreply.github.com`);
+  await exec(`git config --global user.name ${context.actor}`);
+  await exec(
+    `git config --global user.email ${context.actor}@users.noreply.github.com`
+  );
   await exec("yarn version", ["--new-version", newVersion]);
 
   return newVersion;
