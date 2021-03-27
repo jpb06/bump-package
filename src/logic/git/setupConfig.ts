209e9b72e1da2ad8cac8b2e03f7c87c061d772e8
jpb06 <jpb.06@outlook.com>
2021-03-27 11:45:53 +0100

    Appending git config

--- src/logic/git/setupConfig.ts
+++ src/logic/git/setupConfig.ts
@@ -6,6 +6,6 @@ export const setupConfig = async (): Promise<void> => {
   const git = simpleGit({ baseDir: process.cwd() });
 
   await git
-    .addConfig("user.name", `${context.actor}`)
-    .addConfig("user.email", `${context.actor}@users.noreply.github.com`);
+    .addConfig("user.name", `${context.actor}`, true)
+    .addConfig("user.email", `${context.actor}@users.noreply.github.com`, true);
 };
