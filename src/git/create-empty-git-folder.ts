2c084a9255ec5396c40ddfe5ecbd32dee281f1b1
jpb06 <jpb.06@outlook.com>
2025-07-27 20:53:48 +0200

    fix(monorepo): create empty .git folder before running npm version (#56)
    * feat(monorepo): create empty .git folder before running npm version
    
    * fix: missing span attribute

---
+++ src/git/create-empty-git-folder.ts
@@ -0,0 +1,19 @@
+import { FileSystem } from '@effect/platform/FileSystem';
+import { Effect, pipe } from 'effect';
+
+export const createEmptyGitFolder = (cwd: string) =>
+  pipe(
+    Effect.gen(function* () {
+      const { makeDirectory, exists } = yield* FileSystem;
+
+      const gitFolderPath = `${cwd}/.git`;
+
+      const gitFolderPathExists = yield* exists(gitFolderPath);
+      if (gitFolderPathExists) {
+        return;
+      }
+
+      yield* makeDirectory(gitFolderPath);
+    }),
+    Effect.withSpan('create-empty-git-folder', { attributes: { cwd } }),
+  );
